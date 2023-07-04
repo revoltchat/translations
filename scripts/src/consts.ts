0a2e3263eb4e7b430600f0e048a128499acadbd0
Rexogamer <beartechtalks@gmail.com>
2023-07-04 04:15:54 +0100

    feat: push new repo maintainer scripts

---
+++ scripts/src/consts.ts
@@ -0,0 +1,8 @@
+export const ISO_MAP = [
+	{ name: "english", code: "en" },
+	{ name: "french", code: "fr" },
+	{ name: "german", code: "de" },
+	{ name: "ukrainian", code: "uk" },
+	{ name: "russian", code: "ru" },
+	{ name: "japanese", code: "ja" },
+];
